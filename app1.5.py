14d8bbcd1086ae833b2218ebbaeea63bee63a401
Your Name <you@example.com>
2024-07-23 19:17:41 +0900

    complete app.py

--- app1.5.py
+++ app1.5.py
@@ -7,12 +7,14 @@ df = pd.read_csv("data/cases_cumulative_daily.csv",encoding="utf-8")
 
 df["Date"] = pd.to_datetime(df["Date"])
 
-plt.figure(figsize=(8, 3))
+plt.figure(figsize=(9, 4))
 
-plt.plot(df["Date"], df["Iwate"],label="Iwate",color="y")
-plt.plot(df["Date"], df["Tokyo"],label="Tokyo",color="b")
+plt.plot(df["Date"], df["ALL"],label="ALL")
+plt.plot(df["Date"], df["Hokkaido"],label="Hokkaido")
+plt.plot(df["Date"], df["Gifu"],label="Gifu")
+plt.plot(df["Date"], df["Okinawa"],label="Okinawa")
 
-plt.title("岩手・東京-コロナ感染者数")
+plt.title("合計・北海道・岐阜・沖縄-コロナ感染者数")
 plt.xlabel("日付")
 plt.ylabel("感染者数")
 
@@ -21,4 +23,3 @@ plt.xticks(rotation=45)
 
 plt.legend()
 plt.show()
-
